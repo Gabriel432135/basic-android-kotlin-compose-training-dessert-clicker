c63a68264850fde8e8c0365e0a49fca9f91ec208
Gabriel432135 <gabriel432135@hotmail.com>
2026-01-07 20:31:38 -0300

    ViewModel e UiState

---
+++ app/src/main/java/com/example/dessertclicker/ui/DessertUiState.kt
@@ -0,0 +1,9 @@
+package com.example.dessertclicker.ui
+
+class DessertUiState(
+    var revenue: Int = 0,
+    var dessertsSold: Int = 0,
+    val currentDessertIndex: Int = 0,
+    var currentDessertPrice: Int = 0,
+    var currentDessertImageId: Int = 0
+)
